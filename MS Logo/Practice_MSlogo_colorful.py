2bcaa7716f101334b414250c90f18940cbad0ee9
Tetsuyu <v-tetsuyuyo@microsoft.com>
2021-12-12 19:12:25 +0900

    関数定義を使って簡略化

--- MS Logo/Practice_MSlogo_colorful.py
+++ MS Logo/Practice_MSlogo_colorful.py
@@ -1,110 +1,44 @@
 # 色付き MS ロゴ
 # 関数を定義しましょう。
+
 import turtle as t
 t.hideturtle()
 t.speed(150)
+t.width(10)
+
+def main(color1,x,y):
+    t.color(color1)
+    t.penup()
+    t.goto(x,y)
+    t.pendown()
+
+    for a in range(10):
+        t.forward(100)
+        if a % 2 == 0:
+            t.right(90)
+            t.forward(10)
+            t.right(90)
+        else:
+            t.left(90)
+            t.forward(10)
+            t.left(90)
+
+    t.forward(100)
+
+    for b in range(4):
+        t.left(90)
+        t.forward(100)
 
 # 左上 オレンジ
-
-t.color("#F25022")
-t.width(10)
-t.penup()
-t.goto(-110,110)
-t.pendown()
-
-for a in range(10):
-    t.forward(100)
-    if a % 2 == 0:
-        t.right(90)
-        t.forward(10)
-        t.right(90)
-    else:
-        t.left(90)
-        t.forward(10)
-        t.left(90)
-
-t.forward(100)
-
-for b in range(3):
-    t.left(90)
-    t.forward(100)
+main(color1="#F25022", x=-110, y=110)
 
 # 右上 グリーン
-
-t.color("#7FBA00")
-t.width(10)
-t.penup()
-t.goto(10,110)
-t.pendown()
-t.left(90)
-
-for a in range(10):
-    t.forward(100)
-    if a % 2 == 0:
-        t.right(90)
-        t.forward(10)
-        t.right(90)
-    else:
-        t.left(90)
-        t.forward(10)
-        t.left(90)
-
-t.forward(100)
-
-for b in range(3):
-    t.left(90)
-    t.forward(100)
+main(color1="#7FBA00", x=10, y=110)
 
 # 左下 ブルー
-
-t.color("#00A4EF")
-t.width(10)
-t.penup()
-t.goto(-110,-10)
-t.pendown()
-t.left(90)
-
-for a in range(10):
-    t.forward(100)
-    if a % 2 == 0:
-        t.right(90)
-        t.forward(10)
-        t.right(90)
-    else:
-        t.left(90)
-        t.forward(10)
-        t.left(90)
-
-t.forward(100)
-
-for b in range(3):
-    t.left(90)
-    t.forward(100)
+main(color1="#00A4EF", x=-110, y=-10)
 
 # 左下 イエロー
-
-t.color("#FFB900")
-t.width(10)
-t.penup()
-t.goto(10,-10)
-t.pendown()
-t.left(90)
-
-for a in range(10):
-    t.forward(100)
-    if a % 2 == 0:
-        t.right(90)
-        t.forward(10)
-        t.right(90)
-    else:
-        t.left(90)
-        t.forward(10)
-        t.left(90)
-
-t.forward(100)
-
-for b in range(3):
-    t.left(90)
-    t.forward(100)
+main(color1="#FFB900", x=10, y=-10)
 
 t.done()
