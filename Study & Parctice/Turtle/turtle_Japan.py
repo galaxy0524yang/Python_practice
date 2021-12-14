c1b42b92f9adfd8611c2f9ea6ef4f517491db38a
Tetsuyu <v-tetsuyuyo@microsoft.com>
2021-12-14 16:44:41 +0900

    日本国旗を描いてみた

---
+++ Study & Parctice/Turtle/turtle_Japan.py
@@ -0,0 +1,40 @@
+# 日本国旗
+from turtle import *
+
+# turtle プロパティ設定
+length = 1200
+height = 800
+setup(length, height)
+hideturtle()
+tracer(False)
+
+# 白い部分
+penup()
+goto(-375,250)
+pendown()
+color("#000000", "#ffffff")
+
+begin_fill()
+forward(750)
+right(90)
+forward(500)
+right(90)
+forward(750)
+right(90)
+forward(500)
+end_fill()
+
+# 真ん中の赤い円
+penup()
+goto(150, 0)
+pendown()
+color("#FF0000","#FF0000")
+
+begin_fill()
+circle(150)
+end_fill()
+
+ts = getscreen()
+ts.getcanvas().postscript(file = r"C:/PythonProjects/Python_Practice/Study & Parctice/Turtle/日本国旗.eps")
+
+done()
